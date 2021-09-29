1f5a9da644834360e0cdb51bfff7cd1e387df33c
= <=>
2021-09-29 20:50:16 +0900

    CBV로 블로그 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.single_post_page),
-    path('',views.index), # 서버IP/blog
+   # path('<int:pk>/', views.single_post_page),
+   # path('',views.index), # 서버IP/blog
 
+
+    # 뷰를 부르는 부분을 함수가 아니라 클래스로 부름
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('', views.PostList.as_view()),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,48 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
 from .models import Post
 
 # Create your views here.
 
 
-def index(request) :
-    posts = Post.objects.all().order_by('-pk')
+# 템플릿 연결해주는게 뷰 역할
 
-    # template/blog/index.html
-    return render(request, 'blog/index.html',
-                  {
-                      'posts' : posts ,
-                  }
-                  )
+class PostList(ListView) :
+    model = Post
+    ordering = '-pk'  # 최신순으로
+ #   template_name = 'blog/index.html'    # 직접부르기
+ # post_list.html
 
 
-def single_post_page(request, pk) :
-    post = Post.objects.get(pk=pk)
+class PostDetail(DetailView) :
+    model = Post
+# post_detail.html       # model이름_detail.html 이라는 템플릿이 불리어지게 됨. 자동으로?? .  html또 만들필요없다했는데 난 걍 만들어봄
 
-    return render(request, 'blog/single_post_page.html',
-                  {
-                      'post' : post
-                  }
-                  )
+
+
+
+# 함수로 템플릿 불러오기? 위에껀 클래스로 불러오기?
+
+# def index(request) :
+#     posts = Post.objects.all().order_by('-pk')
+#
+#     # template/blog/index.html
+#     return render(request, 'blog/index.html',
+#                   {
+#                       'posts' : posts ,
+#                   }
+#                   )
+#
+#
+# def single_post_page(request, pk) :
+#     post = Post.objects.get(pk=pk)
+#
+#     return render(request, 'blog/single_post_page.html',
+#                   {
+#                       'post' : post
+#                   }
+#                   )
+
+
+
